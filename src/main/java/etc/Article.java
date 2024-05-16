2809963f06f318c3f31a35069ed211a475f71e37
ff1451 <ff1451@gmail.com>
2024-05-16 18:36:50 +0900

    Article 클래스 수정

--- src/main/java/etc/Article.java
+++ src/main/java/etc/Article.java
@@ -1,40 +1,73 @@
 package etc;
 
 public class Article {
-    private int id;
-    private String title;
-    private String content;
+    private int articleId;
+    private String articleTitle;
+    private String articleContent;
+    private String authorId;
+    private String boardId;
+    private String writeDate;
+    private String updatedDate;
 
     public Article() {
     }
 
-    public Article(int id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
+    public Article(int articleId, String articleTitle, String articleContent, String authorId, String boardId,String writeDate) {
+        this.articleId = articleId;
+        this.articleTitle = articleTitle;
+        this.articleContent = articleContent;
+        this.authorId = authorId;
+        this.boardId = boardId;
+        this.writeDate = writeDate;
     }
 
-    public int getId() {
-        return id;
+    public int getarticleId() {
+        return articleId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int articleId) {
+        this.articleId = articleId;
     }
 
-    public String getTitle() {
-        return title;
+    public String getArticleTitle() {
+        return articleTitle;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setArticleTitle(String articleTitle) {
+        this.articleTitle = articleTitle;
     }
 
-    public String getContent() {
-        return content;
+    public String getArticleContent() {
+        return articleContent;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setArticleContent(String articleContent) {
+        this.articleContent = articleContent;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+    public String getBoardId() {
+        return boardId;
+    }
+    public void setBoardId(String boardId) {
+        this.boardId = boardId;
+    }
+    public String getWriteDate() {
+        return writeDate;
+    }
+    public void setWriteDate(String writeDate) {
+        this.writeDate = writeDate;
+    }
+    public String getUpdatedDate() {
+        return updatedDate;
+    }
+    public void setUpdatedDate(String updatedDate) {
+        this.updatedDate = updatedDate;
     }
 }
+
