e14a6eaca24e4b1b85e9428e946ab316da692ec1
ff1451 <ff1451@gmail.com>
2024-05-26 23:21:07 +0900

    API 요구사항 구현

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -7,6 +7,7 @@ import com.example.demo.dto.ArticleResponse;
 import com.example.demo.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PostController {
@@ -15,7 +16,7 @@ public class PostController {
     public PostController(ArticleService articleService) {
         this.articleService = articleService;
     }
-
+/*
     @GetMapping("/posts")
     public String getAllArticles(Model model) {
         List<ArticleResponse> articles = articleService.getAllArticles();
@@ -23,5 +24,12 @@ public class PostController {
         model.addAttribute("boardName", "자유게시판");
         return "articles";
     }
-
+*/
+@GetMapping("/posts")
+public String getArticlesByBoardId(@RequestParam("boardId") Long boardId, Model model) {
+    List<ArticleResponse> articles = articleService.getArticlesByBoardId(boardId);
+    model.addAttribute("articles", articles);
+    model.addAttribute("boardName", "자유게시판"); // Board name should be dynamically retrieved
+    return "articles";
+}
 }
