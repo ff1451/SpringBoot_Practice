fbda302ffdf056e35c990baca81bb1cfd62c0e20
ff1451 <ff1451@gmail.com>
2024-05-27 12:25:41 +0900

    [Modify] 인터페이스, 클래스 단일화

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -1,13 +1,37 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Board;
+import com.example.demo.repository.BoardRepository;
+import com.example.demo.repository.MemoryBoardRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface BoardService {
-    Board createBoard(Board board);
-    Board getBoardById(Long id);
-    List<Board> getAllBoards();
-    Board updateBoard(Board board);
-    void deleteBoard(Long id);
+@Service
+public class BoardService {
+    private final BoardRepository boardRepository = new MemoryBoardRepository();
+
+    public Board createBoard(Board board) {
+        return boardRepository.createBoard(board);
+    }
+
+    public Board getBoardById(Long id) {
+        return boardRepository.findBoardById(id)
+                .orElseThrow(()->new IllegalArgumentException("게시판 id 조회 실패: "+ id));
+    }
+
+    public List<Board> getAllBoards() {
+        return boardRepository.findAllBoards();
+    }
+
+    public Board updateBoard(Board board) {
+        if (board.getId() == null) {
+            throw new IllegalArgumentException("게시판 id 조회 실패");
+        }
+        return boardRepository.updateBoard(board);
+    }
+
+    public void deleteBoard(Long id) {
+        boardRepository.deleteBoard(id);
+    }
 }
--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,13 +1,37 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface MemberService {
-    Member join(Member member);
-    Member getMemberById(Long id);
-    List<Member> getAllMembers();
-    Member updateMember(Member member);
-    void deleteMember(Long id);
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Member join(Member member) {
+        return memberRepository.createMember(member);
+    }
+
+    public Member getMemberById(Long id) {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("회원 id 조회 실패: "+ id));
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAllMember();
+    }
+
+    public Member updateMember(Member member) {
+        if(member.getId()==null) {
+            throw new IllegalArgumentException("회원 id 조회 실패");
+        }
+        return memberRepository.updateMember(member);
+    }
+
+    public void deleteMember(Long id) {
+        memberRepository.deleteById(id);
+    }
 }
